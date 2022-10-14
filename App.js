ad54f68045482fd1269defa2777b374a12163e33
HongRok Kim <dev@hongrok.net>
2022-10-14 11:02:45 +0900

    프로덕션 빌드

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { WebView } from "react-native-webview";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <WebView source={{ uri: "https://classbank.kr/app" }} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f4f4f4",
   },
 });
